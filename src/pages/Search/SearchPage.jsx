3a3f474d34019ddd3f57ac2ac9bee458aec29fe0
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-10-13 02:57:10 +0900

    feat: 검색 api 연동

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -1,5 +1,27 @@
+import { useQuery } from "@tanstack/react-query";
+import { getSearchPhotos } from "../../api/search";
+import { useLocation } from "react-router-dom";
+
 function SearchPage() {
-  return <>으하하하하하하</>;
+  const location = useLocation();
+  const search = location.search.split("?query=")[1];
+
+  const { data, isLodding, error } = useQuery({
+    queryKey: ["search", search],
+    queryFn: () => getSearchPhotos(decodeURIComponent(search)), // api를 요청하는 함수를 넣기
+  });
+
+  return (
+    <>
+      {data?.results.map((data) => {
+        return (
+          <div key={data.urls.regular}>
+            <img src={data.urls.regular} alt="" />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default SearchPage;
