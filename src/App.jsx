0d76b17f83ea475d73b3045be70a6b10ae5800ea
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-10-12 17:07:56 +0900

    Revert "feat: 텐스텍쿼리 기본 설정 및 서버포트 설정 추가"

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/Header";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
+    <>
       <Header />
       <main></main>
-    </QueryClientProvider>
+    </>
   );
 }
 
