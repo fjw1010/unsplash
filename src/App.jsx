4068041736df0ab9e7cac843fc4f577a9052678e
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-10-13 00:24:36 +0900

    feat: axios 세팅 변경, StrictMode App.jsx 태그로 다시 랩핑

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/Header";
+import { StrictMode } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Header />
-      <main></main>
-    </QueryClientProvider>
+    <StrictMode>
+      <QueryClientProvider client={queryClient}>
+        <Header />
+        <main></main>
+      </QueryClientProvider>
+    </StrictMode>
   );
 }
 
