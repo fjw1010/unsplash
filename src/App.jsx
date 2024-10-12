278e805f03de43e9b81d32f3b2d36a2451aa62a1
Hanso <167108402+fjw1010@users.noreply.github.com>
2024-10-12 17:09:33 +0900

    Reapply "feat: 텐스텍쿼리 기본 설정 및 서버포트 설정 추가"
    This reverts commit 0d76b17f83ea475d73b3045be70a6b10ae5800ea.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "./components/Header";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <Header />
       <main></main>
-    </>
+    </QueryClientProvider>
   );
 }
 
