1ea5b471c43b0bf25f8d357d89870bc326e2ad68
Hanso <fjw_1010@naver.com>
2024-10-18 23:57:41 +0900

    feat: 무한 스크롤 구현 중

--- src/pages/Search/SearchPage.jsx
+++ src/pages/Search/SearchPage.jsx
@@ -1,19 +1,37 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import { getSearchPhotos } from "../../api/search";
 import { useLocation } from "react-router-dom";
+import { useInView } from "react-intersection-observer";
+import styled from "styled-components";
+import { useEffect } from "react";
 
 function SearchPage() {
   const location = useLocation();
   const search = location.search.split("?query=")[1];
+  const [ref, inView] = useInView({});
 
-  const {
-    data = [],
-    isLodding,
-    error,
-  } = useQuery({
-    queryKey: ["search", search],
-    queryFn: () => getSearchPhotos(decodeURIComponent(search)), // api를 요청하는 함수를 넣기
+  const { data, fetchNextPage, isLoading } = useInfiniteQuery({
+    queryKey: ["infiniteSearch"],
+    queryFn: ({ pageParam = 1 }) =>
+      getSearchPhotos(decodeURIComponent(search), pageParam),
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.total_pages > pages.length) {
+        return pages.length + 1;
+      }
+      return undefined;
+    },
   });
+
+  //
+  useEffect(() => {
+    if (inView) {
+      fetchNextPage();
+    }
+  }, [inView, fetchNextPage]);
+
+  if (isLoading) {
+    return <div>로딩 중 입니다...</div>;
+  }
 
   return (
     <>
@@ -24,8 +42,10 @@ function SearchPage() {
           </div>
         );
       })}
+      <Div ref={ref} />
     </>
   );
 }
 
+const Div = styled.div``;
 export default SearchPage;
